c18afac0ebdc29ed280d94d3a79c60fd31139918
Lydia B <lydiabaik@gmail.com>
2020-01-30 08:08:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,10 @@ require 'pry'
 class Song
   attr_accessor :name
   attr_reader :artist
-  extend Memorable
   extend Findable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+
   include Paramable
 
   @@songs = []
